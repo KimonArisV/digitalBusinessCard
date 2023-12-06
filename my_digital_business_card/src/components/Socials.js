3d0ce079aed5af70af3c83ca51b0332f4c0eeb4f
Kimon Vogt <kvogt@g.harvard.edu>
2023-12-06 09:20:24 -0500

    added the href a tags for socials and my info as well as connected the icons

--- my_digital_business_card/src/components/Socials.js
+++ my_digital_business_card/src/components/Socials.js
@@ -1,9 +1,16 @@
 import React from "react";
+import GithubIcon from "../images/github.png";
+import Xtwitter from "../images/X.png";
 
 export default function SocialsInfo() {
     return(
-        <footer>
-            <h4>hellow</h4>
+        <footer className="fotter-social">
+            <a href="https://github.com/KimonArisV">
+                <img src={GithubIcon} alt="icon of LinkedIn" />
+            </a>
+            <a href="https://twitter.com/KimonV99">
+                <img src={Xtwitter} alt="icon of X"/>
+            </a>   
         </footer>
     )
 }
